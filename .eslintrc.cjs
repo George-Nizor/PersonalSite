a066484a244c7052f01a0be40a10c3c6a7faf00e
George Nizor <gnizorid@gmail.com>
2025-09-17 13:56:23 +1000

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['astro'],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        'astro/no-set-html-directive': 'warn'
+      }
+    }
+  ],
+  rules: {}
+};
---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://georgenizoridis.com',
+  integrations: [tailwind({ applyBaseStyles: false })],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
+});
