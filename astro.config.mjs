c94079d60b62565ee3d0e0ec3cb4fdd9fc372f65
George Nizor <gnizorid@gmail.com>
2025-09-17 17:33:53 +1000

    Deployment Prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://georgenizoridis.com',
+  output: 'static',
+  adapter: undefined,
   integrations: [tailwind({ applyBaseStyles: false })],
   server: {
     watch: {
